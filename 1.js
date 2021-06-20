e1024c3905e19affaef21a7c147e6626c606d21c
Dmitriy <2cam@mail.ru>
2021-06-20 16:47:22 +0300

    Дни недели

---
+++ 1.js
@@ -0,0 +1,67 @@
+// 1. Найти произведение цифр натурального числа
+
+    /* let num = 33721;
+    let a = 0;
+    let m = 1;
+    let arr = [];
+
+    let i = 0;
+
+    while (num > 0){
+        
+        a = num % 10;
+        arr[i] = a;
+        num = (num - a) / 10;
+        i++;
+    }
+
+    for (let k = 0; k < arr.length; k++) {
+    
+        m = m * arr[k];
+    }
+
+    console.log(arr);
+    console.log(m); */
+
+// 2. Полученный результат возвести в степень 3, используя только 1 оператор
+//        Вывести его на экран
+
+    //console.log(m**3);
+
+// 3. Доп. задачи
+
+
+    //let x = 5; alert( x++ ); 
+
+    //let x = [ ] + false - null + true ;
+    //console.log(x);
+
+    //let y = 1; 
+    //let x = y = 2; 
+    //alert(x); 
+    
+    //let a = [ ] + 1 + 2;
+    //console.log(typeof(a))
+
+    //alert( "1"[0] );
+
+    //console.log(2 && 1 && null && 0 && undefined );
+    
+    /* let a = 12;
+    let b = 12;
+    console.log(!!(a && b) == (a && b)); */
+
+    //alert( null || 1 && 5 || 4 ); 
+    
+    /* let a = [1, 2, 3]; 
+    let b = [1, 2, 3]; 
+    console.log(a == b); */
+
+    //alert( +"Infinity" ); 
+
+    //console.log("ёжик" > "яблоко");
+
+    //console.log(0 || "" || 2 || undefined || true || falsе );
+
+//
+
--- javascript.js
+++ javascript.js
@@ -1,65 +1,20 @@
-// 1. Найти произведение цифр натурального числа
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
-    /* let num = 33721;
-    let a = 0;
-    let m = 1;
-    let arr = [];
+let out_arr = document.querySelector('#out_arr');
 
-    let i = 0;
+let str = '';
 
-    while (num > 0){
-        
-        a = num % 10;
-        arr[i] = a;
-        num = (num - a) / 10;
-        i++;
+for (let i = 0; i< week.length; i++  ) {
+    if (week[i]!==undefined) {
+        if (week[i] == 'Суббота' || week[i] == 'Воскресенье') {
+            str += '<b>' + week[i]+'<br>' + '</b>';
+        } else if (week[i] == 'Среда') {
+            str += '<i>' + week[i]+'<br>' + '</i>';
+        } else {
+            str +=  week[i]+'<br>';
+        }       
     }
+}
+out_arr.innerHTML = str;
 
-    for (let k = 0; k < arr.length; k++) {
-    
-        m = m * arr[k];
-    }
-
-    console.log(arr);
-    console.log(m); */
-
-// 2. Полученный результат возвести в степень 3, используя только 1 оператор
-//        Вывести его на экран
-
-    //console.log(m**3);
-
-// 3. Доп. задачи
-
-
-    //let x = 5; alert( x++ ); 
-
-    //let x = [ ] + false - null + true ;
-    //console.log(x);
-
-    //let y = 1; 
-    //let x = y = 2; 
-    //alert(x); 
-    
-    //let a = [ ] + 1 + 2;
-    //console.log(typeof(a))
-
-    //alert( "1"[0] );
-
-    //console.log(2 && 1 && null && 0 && undefined );
-    
-    /* let a = 12;
-    let b = 12;
-    console.log(!!(a && b) == (a && b)); */
-
-    //alert( null || 1 && 5 || 4 ); 
-    
-    /* let a = [1, 2, 3]; 
-    let b = [1, 2, 3]; 
-    console.log(a == b); */
-
-    //alert( +"Infinity" ); 
-
-    //console.log("ёжик" > "яблоко");
-
-    //console.log(0 || "" || 2 || undefined || true || falsе );
 
